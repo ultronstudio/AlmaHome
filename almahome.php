d83a6c524545845efceb31508643b8e97974aa85
Petr Vurm <p.vurm@outlook.cz>
2024-03-04 15:38:29 +0100

    selfupdate

--- almahome.php
+++ almahome.php
@@ -77,6 +77,66 @@ function register_almahome_styles() {
 	wp_enqueue_style('almahome-style', plugin_dir_url(__FILE__) . 'css/custom-styles.css');
 }
 
+/**
+ * Check for updates to this plugin
+ *
+ * @param array  $update   Array of update data.
+ * @param array  $plugin_data Array of plugin data.
+ * @param string $plugin_file Path to plugin file.
+ * @param string $locales    Locale code.
+ *
+ * @return array|bool Array of update data or false if no update available.
+ */
+function self_update( $update, array $plugin_data, string $plugin_file, $locales ) {
+	// only check this plugin
+	if ( 'AlmaHome/almahome.php' !== $plugin_file ) {
+		return $update;
+	}
+
+	// already completed update check elsewhere
+	if ( ! empty( $update ) ) {
+		return $update;
+	}
+
+	// let's go get the latest version number from GitHub
+	$response = wp_remote_get(
+		'https://api.github.com/repos/ultronstudio/AlmaHome/releases/latest',
+		array(
+			'user-agent' => 'ultronstudio',
+		)
+	);
+
+	if ( is_wp_error( $response ) ) {
+		return;
+	} else {
+		$output = json_decode( wp_remote_retrieve_body( $response ), true );
+	}
+
+	$new_version_number  = $output['tag_name'];
+	$is_update_available = version_compare( $plugin_data['Version'], $new_version_number, '<' );
+
+	if ( ! $is_update_available ) {
+		return false;
+	}
+
+	$new_url     = $output['html_url'];
+	$new_package = $output['assets'][0]['browser_download_url'];
+
+	error_log('$plugin_data: ' . print_r( $plugin_data, true ));
+	error_log('$new_version_number: ' . $new_version_number );
+	error_log('$new_url: ' . $new_url );
+	error_log('$new_package: ' . $new_package );
+
+	return array(
+		'slug'    => $plugin_data['TextDomain'],
+		'version' => $new_version_number,
+		'url'     => $new_url,
+		'package' => $new_package,
+	);
+}
+
+add_filter( 'update_plugins_github.com', 'self_update', 10, 4 );
+
 add_filter( 'auto_update_plugin', '__return_true' );
 add_filter( 'plugin_row_meta', 'plugin_meta_links', 10, 4 );
 
